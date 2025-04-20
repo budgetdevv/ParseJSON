f7a92a5e6104fad6315ec5ef0314c2e11564762f
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2025-04-20 11:39:07 +0800

    Initial commit

---
+++ ParseJSON/Program.cs
@@ -0,0 +1,30 @@
+﻿using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace ParseJSON
+{
+    public static class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.Write("Input the path to your JSON file:");
+
+            var json = await File.ReadAllTextAsync(Console.ReadLine()!);
+
+            var schema = JsonSchema.FromSampleJson(json);
+
+            var settings = new CSharpGeneratorSettings
+            {
+                Namespace = "Foo"
+            };
+
+            var generator = new CSharpGenerator(schema, settings);
+
+            var classCode = generator.GenerateFile();
+
+            await File.WriteAllTextAsync("GeneratedClasses.cs", classCode);
+
+            Console.WriteLine("C# classes generated successfully.");
+        }
+    }
+}
